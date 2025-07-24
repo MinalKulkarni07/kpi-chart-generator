8efbd05263b2767c7c24545f33b86d4f3b0eb985
Minal Kulkarni <minalkulkarni604@gmail.com>
2025-07-24 21:25:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,12 +49,11 @@ def main():
 
 
 def looks_like_date(val):
-    if st.session_state.data is not None:
-        try:
-            parse(str(val))
-            return True
-        except:
-            return False
+    try:
+        parse(str(val))
+        return True
+    except:
+        return False
 
      
     if page == "📁 Data Upload":
