ba160340eca7cefcad7ddc638873abd970991a51
Minal Kulkarni <minalkulkarni604@gmail.com>
2025-07-29 21:41:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,19 +15,21 @@ from help_guide import help_guide_page
 from welcome import show_lottie_welcome
 import streamlit.components.v1 as components
 
-def inject_gtm():
-    components.html(
-        """
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MLQQSDSF');</script>
-        <!-- End Google Tag Manager -->
-        """,
-        height=0
-    )
+# Inject Google Tag Manager (GTM) manually
+st.markdown("""
+<!-- Google Tag Manager -->
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLQQSDSF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+<script>
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MLQQSDSF');
+</script>
+<!-- End Google Tag Manager -->
+""", unsafe_allow_html=True)
+
 
 
 
@@ -57,7 +59,6 @@ if 'selected_columns' not in st.session_state:
 
 
 def main():
-    inject_gtm()
     show_lottie_welcome()
     st.title("📊 :red[KPI] & :rainbow[Chart] Generator")
     st.markdown("Upload your CSV file and generate interactive dashboards with key performance indicators and visualizations.")
