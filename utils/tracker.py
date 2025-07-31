121f16811b371c0ef0590afbf27127b7f1efdf07
Minal Kulkarni <minalkulkarni604@gmail.com>
2025-07-31 21:27:47 +0530

    Update tracker.py

--- utils/tracker.py
+++ utils/tracker.py
@@ -12,3 +12,16 @@ def log_to_google_sheets(event, page, user_info="anonymous", notes=""):
         requests.post(url, json=payload, timeout=3)
     except Exception as e:
         print("Logging failed:", e)
+
+def get_user_location():
+    try:
+        response = requests.get("https://ipinfo.io/json", timeout=3)
+        if response.status_code == 200:
+            data = response.json()
+            city = data.get("city", "")
+            region = data.get("region", "")
+            country = data.get("country", "")
+            return f"{city}, {region}, {country}"
+    except:
+        pass
+    return "Unknown"
